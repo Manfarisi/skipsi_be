49646e238a31c462f9597f88748e511be9b18ea8
Salman <kingmansalman@gmail.com>
2025-07-31 13:37:47 +0700

    fix: <public/images>

--- middleware/upploadMiddleware.js
+++ middleware/upploadMiddleware.js
@@ -1,9 +1,16 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+// Pastikan folder public/images ada
+const dir = "public/images";
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images"); // folder penyimpanan
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + "-" + file.originalname;
